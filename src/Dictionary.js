158d6d826624eaacc9f70580b895ed5fdcb47282
emmaleecamargo <emmaleecamargo@gmail.com>
2023-02-17 10:39:25 +0000

    fixing API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,15 +9,14 @@ export default function Dictionary() {
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
 
-    let key = "b54a44de2tffce2f3d9fb2ob8990686c";
-
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
