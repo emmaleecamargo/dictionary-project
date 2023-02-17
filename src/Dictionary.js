4a5ea6f9245d8d4dc326dda266c76e249da1eeff
emmaleecamargo <emmaleecamargo@gmail.com>
2023-02-17 10:44:49 +0000

    removed undeclared

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,11 +5,9 @@ import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
-    setResults(response.data[0]);
   }
 
   function search(event) {
