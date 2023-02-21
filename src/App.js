4a0ac7d8a965e74310554253f3916f56da600722
emmaleecamargo <emmaleecamargo@gmail.com>
2023-02-21 23:07:10 +0000

    fixing api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -5,10 +6,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="text-center">coded by Emmalee</footer>
+        <footer className="App-footer">
+          <small>Coded by Emmalee</small>
+        </footer>
       </div>
     </div>
   );
